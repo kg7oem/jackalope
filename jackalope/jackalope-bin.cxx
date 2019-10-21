30a1f425d9376fc8e99095b698af2b2b4ae41dc3
Tyler Riddle <cardboardaardvark@gmail.com>
2019-10-20 21:06:48 -0700

    set example of how to use change when done

--- jackalope/jackalope-bin.cxx
+++ jackalope/jackalope-bin.cxx
@@ -39,48 +39,42 @@ int main(int argc_in, char ** argv_in)
 
     jackalope_init();
 
-    auto domain = make_audio_domain({
-        { "node:name", "main domain" },
-        { "audio:sample_rate", to_string(48000) },
-        { "audio:buffer_size", to_string(128) },
-        { "input:left input", "pcm[real]" },
-        { "input:right input", "pcm[real]" },
+    auto graph = make_graph({
+        { "pcm:sample_rate", to_string(48000) },
+        { "pcm:buffer_size", to_string(128) },
     });
 
-    domain->activate();
-
-    auto system_audio = domain->make_driver({
-        { "node:class", "audio::driver::portaudio" },
+    auto system_audio = graph->add_node({
+        { "node:class", "pcm::driver::portaudio" },
         { "node:name", "system audio" },
-    });
+    );
 
-    auto input_file = domain->make_node({
-        { "node:class", "audio::node::sndfile" },
+    auto input_file = graph->add_node({
+        { "node:class", "pcm::node::sndfile" },
         { "node:name", "input file" },
         { "config:path", argv_in[1] },
     });
 
-    auto left_tube = domain->make_node({
-        { "node:class", "audio::node::ladspa" },
+    auto left_tube = graph->add_node({
+        { "node:class", "pcm::node::ladspa" },
         { "node:name", "left tube" },
         { "plugin:id", to_string(LADSPA_ZAMTUBE_ID) },
     });
 
-    auto right_tube = domain->make_node({
-        { "node:class", "audio::node::ladspa" },
+    auto right_tube = graph->add_node({
+        { "node:class", "pcm::node::ladspa" },
         { "node:name", "right tube" },
         { "plugin:id", to_string(LADSPA_ZAMTUBE_ID) },
     });
 
-    input_file->get_signal("file:eof")->connect(domain->get_slot("system:terminate"));
+    input_file->get_signal("file:eof")->connect(graph->get_slot("system:shutdown"));
 
     input_file->get_output("output 1")->link(left_tube->get_input("Audio Input 1"));
     input_file->get_output("output 1")->link(right_tube->get_input("Audio Input 1"));
-    left_tube->get_output("Audio Output 1")->link(domain->get_input("left input"));
-    right_tube->get_output("Audio Output 1")->link(domain->get_input("right input"));
+    left_tube->get_output("Audio Output 1")->link(system_audio->get_input("left input"));
+    right_tube->get_output("Audio Output 1")->link(system_audio->get_input("right input"));
 
-    domain->start();
-    system_audio->start();
+    graph->start();
 
     while(1) {
         using namespace std::chrono_literals;
