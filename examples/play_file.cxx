ed5d854acf65f593ace9a763d19bd50cae762247
Tyler Riddle <cardboardaardvark@gmail.com>
2020-02-05 08:56:00 -0800

    RtAudio seems to be the source of the segfaults

--- examples/play_file.cxx
+++ examples/play_file.cxx
@@ -51,7 +51,7 @@ int main(int argc_in, char ** argv_in)
     });
 
     auto system_audio = graph.add_node({
-        { "object.type", "audio::rtaudio" },
+        { "object.type", "audio::portaudio" },
         { "node.name", "system audio" },
         { "sink.left", "audio" },
         { "sink.right", "audio" },
